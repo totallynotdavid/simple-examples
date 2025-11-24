99f498bc7e6b68d01192c860744a3ff27d1f2791
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-11-24 13:09:57 -0500

    feat: set base path for deployment to /simple-examples/

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/simple-examples/",
   root: path.resolve(__dirname, "src"),
   plugins: [react(), tailwindcss()],
   resolve: {
